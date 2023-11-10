9977ebac5a03edb2c52330570079c856d9d36919
Комаров Семён Андреевич <sam@skomarov.com>
2023-11-10 08:58:56 +0300

    implement Register handler

--- internal/delivery/server.go
+++ internal/delivery/server.go
@@ -2,18 +2,28 @@ package delivery
 
 import (
 	"auth-gateway-svc/internal/config"
+	"auth-gateway-svc/internal/core"
+	"auth-gateway-svc/internal/service"
+	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"net/http"
 )
+
+type IAuthService interface {
+	Register(login, password string) (*service.Tokens, error)
+	Login(login, password string) (*service.Tokens, error)
+}
 
 type Server struct {
 	cfg       config.HTTPServer
 	forwarder http.Handler
 	r         chi.Router
+	svc       IAuthService
 }
 
-func NewServer(cfg config.HTTPServer, forwarder http.Handler) http.Handler {
-	srv := &Server{cfg, forwarder, chi.NewRouter()}
+func NewServer(cfg config.HTTPServer, forwarder http.Handler, svc IAuthService) http.Handler {
+	srv := &Server{cfg, forwarder, chi.NewRouter(), svc}
 	srv.defineEndpoints()
 	return srv
 }
@@ -25,7 +35,20 @@ func (s *Server) defineEndpoints() {
 }
 
 func (s *Server) Register(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Register"))
+	var authReq AuthRequest
+	if err := json.NewDecoder(r.Body).Decode(&authReq); err != nil {
+		core.WriteErrorResponse(w, &core.ClientError{
+			DisplayMessage: fmt.Sprintf("while decoding request: %v", err),
+			HTTPCode:       0,
+		})
+		return
+	}
+	tokens, err := s.svc.Register(authReq.Login, authReq.Password)
+	if err != nil {
+		core.WriteErrorResponse(w, err)
+		return
+	}
+	json.NewEncoder(w).Encode(tokens)
 }
 
 func (s *Server) SignIn(w http.ResponseWriter, r *http.Request) {
